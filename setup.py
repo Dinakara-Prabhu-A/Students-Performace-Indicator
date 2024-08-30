ba8472cc2343e821dde61c3156b1a6c3458d8ba5
Dinakara-Prabhu-A <160638694+Dinakara-Prabhu-A@users.noreply.github.com>
2024-08-30 07:58:34 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requires() -> List[str]:
+    with open('requirements.txt', 'r') as file: 
+        requirements = file.read().splitlines()
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name = 'E Commerce Customer Segmentation',
+    version = '0.1',
+    author = 'Dinakara Prabhu A',
+    author_email = 'dinakaraprabhu11@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requires()
+)
